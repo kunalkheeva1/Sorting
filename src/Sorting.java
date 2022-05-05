ab1f57ad571d127839f44eff0029415513ea2a85
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-04 21:32:11 -0700

    bubble sort

---
+++ src/Sorting.java
@@ -0,0 +1,10 @@
+public class Sorting {
+    public static void main(String[] args) {
+        int[] numbers={2,5,3,18,4,6};
+        for(int i=0; i<numbers.length-1; i++){
+            for(int j=0; j<numbers.length-i-1; i++){
+                if(numbers[i]>numbers[i+1]){             //swap
+            }
+        }
+    }
+}
