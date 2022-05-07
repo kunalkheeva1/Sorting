cc8743743763cad6182df9b64ad721dd08fe3dc3
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-06 19:53:03 -0700

    insertion sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,16 +1,15 @@
-public class Sorting{
+public class Sorting {
     public static void main(String[] args) {
-        int []arr={2,5,3,8,7,36,53,10,43,655,32,34,54,245,4235,2436};
+        int []arr ={3,45,5,63,66,6,94,2};
+        for(int i=1; i<arr.length; i++){
+            int current =arr[i];
+            int j=i-1;
+            while(j>=0 && current<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }arr[j+1]=current;
+        }
         for(int i=0; i<arr.length; i++){
-            int smallest =i;
-            for(int j=i; j<arr.length; j++){
-                if(arr[i]>arr[j]){
-                    smallest=j;
-                }
-            }int temp = arr[smallest];
-            arr[smallest]=arr[i];
-            arr[i]=temp;
-        }for(int i=0; i<arr.length; i++){
             System.out.print(arr[i] +" ");
         }
     }
